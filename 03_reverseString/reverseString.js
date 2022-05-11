cfe860ca88e05c61668ab3e89e67f5bc3c894d40
Ishpaul Singh <ishpaul207@gmail.com>
2022-05-11 21:08:57 +0530

    03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = string.split("");
+    let reversed = []
+    for (i = result.length - 1; i >= 0; i--){
+        reversed.push(result[i]);
+    }
+    let answer = reversed.join('')
+    return answer
 };
 
 // Do not edit below this line
