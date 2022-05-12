6e0050d10aaa2e03e30c8df52b76bae7f08fe9a8
Ishpaul Singh <ishpaul207@gmail.com>
2022-05-12 12:11:20 +0530

    04 05 exercises completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let argues = Array.from(arguments);//it converts arguements into a array
+    let arr = argues[0];//this select the array from which items to be removed
+    let toBeRemoved = argues.slice(1,argues.length) //this makes an array of items to be removed
+    let result = []// an empty array which will be retuer
+    for(i=0;i<arr.length;i++){//this loops over items of array from which we have to remove selected items 
+            if(!toBeRemoved.includes(arr[i])){ 
+              result.push(arr[i]);
+            }
+        
+        }return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let result = 0
+    if(num1 < 0 || num2 < 0 || typeof num1 !== "number"  || typeof num2 !== "number"){
+        return 'ERROR'
+    }
+    if(num1 < num2){
+    for(i = num1; i <= num2; i++){
+    result += i
+    }return result}else{
+        for(i = num2; i <= num1; i++){
+        result += i
+        }return result}
 };
 
 // Do not edit below this line
